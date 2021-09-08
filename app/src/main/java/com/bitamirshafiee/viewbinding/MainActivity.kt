5909a4e5c80f066937ce1536b20bb6d883d7f1bf
YeshayahuBennun <ybennun@yahoo.com>
2021-09-08 22:42:18 +0300

    commit#2 - ActivityMainBinding and layoutInflater

--- app/src/main/java/com/bitamirshafiee/viewbinding/MainActivity.kt
+++ app/src/main/java/com/bitamirshafiee/viewbinding/MainActivity.kt
@@ -2,11 +2,27 @@ package com.bitamirshafiee.viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.bitamirshafiee.viewbinding.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
+
+        binding.textView.text = resources.getString(R.string.str_text_view)
+        binding.buttonMessage.setOnClickListener{
+            Toast.makeText(this,resources.getText(R.string.str_button_is_clicked),Toast.LENGTH_SHORT).show()
+        }
     }
 }
